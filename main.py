ae2f72212868bdc8304f80ce3b7a1cbf155481c3
chaithanyatenneti <chaithanya.tenneti@gmail.com>
2024-10-12 00:18:28 +0200

    Model evaluation with mlflow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Classification import logger
 from Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from Classification.pipeline.stage_03_model_trainer import ModelTrainerPipeline
+from Classification.pipeline.stage_04_model_evaluation_with_mlflow import ModelEvaluationPipeline
 
 STAGE_NAME = "Data Ingestion "
 
@@ -21,6 +23,28 @@ try:
     obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")  
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Training"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelEvaluationPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
